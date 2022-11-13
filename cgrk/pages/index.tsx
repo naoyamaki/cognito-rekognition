666575260ceb078e79e04092d24c7fd5310ff7d1
naoyamaki <stringbassandsticks@gmail.com>
2022-11-13 13:00:51 +0900

    react-webcamを使いカメラから写真を撮る機能を追加

--- cgrk/pages/index.tsx
+++ cgrk/pages/index.tsx
@@ -1,20 +1,66 @@
+import { useRef, useState, useCallback } from "react";
 import type { NextPage } from 'next';
 import { signIn, signOut, useSession } from 'next-auth/react';
+import Webcam from "react-webcam";
+
+const videoConstraints = {
+  width: 540,
+  height: 360,
+  facingMode: "user"
+};
 
 const Home: NextPage = () => {
   const { data: session } = useSession();
+  const [isCaptureEnable, setCaptureEnable] = useState<boolean>(false);
+  const webcamRef = useRef<Webcam>(null);
+  const [url, setUrl] = useState<string | null>(null);
+  const capture = useCallback(() => {
+    const imageSrc = webcamRef.current?.getScreenshot();
+    if (imageSrc) {
+      setUrl(imageSrc);
+    }
+  }, [webcamRef]);
+
   if (session && session.user) {
     return (
-      <>
-        Signed in as {session.user.email} <br />
-        <button onClick={() => signOut()}>Sign out</button>
-        
-      </>
-    );
-  }
+        <>
+          <header>
+            <h1>カメラアプリ</h1>
+          </header>
+          {isCaptureEnable || (
+            <button onClick={() => setCaptureEnable(true)}>開始</button>
+          )}
+          {isCaptureEnable && (
+            <>
+              <button onClick={() => setCaptureEnable(false)}>終了</button>
+              <Webcam
+                audio={false}
+                width={540}
+                height={360}
+                ref={webcamRef}
+                screenshotFormat="image/jpeg"
+                videoConstraints={videoConstraints}
+              />
+              <button onClick={capture}>キャプチャ</button>
+            </>
+          )}
+          {url && (
+            <>
+              <button
+                onClick={() => {
+                  setUrl(null);
+                }}
+              >
+                削除
+              </button>
+              <img src={url} alt="Screenshot" />
+            </>
+          )}
+        </>
+    )};
   return (
     <>
-      Not signed in <br />
+      <p>Not signed in</p>
       <button onClick={() => signIn()}>Sign in</button>
     </>
   );
